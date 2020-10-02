01548da5bde8403466cad4bb793f9bbb616660b0
Xiscoder <turnbjos@mail.gvsu.edu>
2020-10-02 02:11:30 -0400

    flask backend

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
+        ... no changes in this part ...
 
         <p>The current time is {currentTime}.</p>
       </header>
